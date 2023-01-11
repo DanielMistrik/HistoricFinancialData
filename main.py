8f8644b5decb776e3feb2e9a561363569b9f9bec
Daniel Mistrik <33633302+DanielMistrik@users.noreply.github.com>
2023-01-11 23:46:04 +0100

    Adding the core repurposed revenue data and some basic infrastructure to support other data pulls (Tested but only a bit)

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+import requests
+import json
+import numpy as np
+import utils as ut
+from exceptions import *
+
+"""
+main.py - 
+"""
+
+
 class FinData:
+    # Defining fields to access and extract data from the SEC API
+    _revenue_jargon = ["SalesRevenueNet", "RevenueFromContractWithCustomerExcludingAssessedTax", "SalesRevenueGoodsNet",
+                       "Revenues", "RevenueNet", "RevenuesNet"]
+    _cik_map_url = "https://www.sec.gov/files/company_tickers.json"
+    _ticker_cik_map = {}
+    _name_cik_map = {}
+    _cik_ticker_map = {}
+
+    "Helper function that fills in the various cik maps the SEC uses to classify company financials"
+    def _fill_cik_map(self):
+        r = requests.get(self._cik_map_url)
+        json_output = json.loads(r.content.decode('utf-8'))
+        i = 0
+        while str(i) in json_output:  # The SEC returns an output of a dictionary with string numbers as keys
+            ticker = json_output[str(i)]["ticker"]
+            company_cir = str(json_output[str(i)]["cik_str"])
+            company_cir = "0"*(10-len(company_cir))+company_cir # To make it compatible with SEC API calls
+            self._ticker_cik_map[ticker] = company_cir
+            self._cik_ticker_map[company_cir] = ticker
+            i += 1
+
     def __init__(self):
-        pass
+        # Fills in mapping from human-understandable tickers to SEC identification numbers
+        self._fill_cik_map()
+
+    def get_revenue(self, ticker, start_year=0, start_quarter=0, end_year=3000, end_quarter=5):
+        cik = self._ticker_cik_map[ticker]
+        return ut.get_data(cik, self._revenue_jargon, 'Revenue', start_year, start_quarter, end_year, end_quarter)
+
