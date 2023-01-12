0bc5360587485fad4d1b1d433cf50576c75c19f2
Daniel Mistrik <33633302+DanielMistrik@users.noreply.github.com>
2023-01-12 12:45:15 +0100

    Making the test class actually run (fixing error with constructor)

--- test_main.py
+++ test_main.py
@@ -1,9 +1,14 @@
 from unittest import TestCase
 from main import FinData
 
+
 class TestFinData(TestCase):
     fin_data_test_subject = None
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
+        super(TestFinData, self).__init__(*args, **kwargs)
         self.fin_data_test_subject = FinData()
+
     def test_get_revenue(self):
-        self.fail()
+        revenue_data = self.fin_data_test_subject.get_revenue('AAPL',2010,1,2022,4)
+        self.assertEqual(len(revenue_data), 51, "50 quarters between 2010 and 2022 plus the column names")
